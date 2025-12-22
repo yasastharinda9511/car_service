002a51a085b98df9b023aa6c44d98c12ed9760d1
yasastharinda9511 <yasastharinda9511@gmail.com>
2025-12-23 00:12:41 +0530

    added debug lines

--- config/cors.go
+++ config/cors.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -18,6 +19,11 @@ func (c *CorsConfig) WithCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 
+		fmt.Println("called is for origin: ", origin)
+
+		for _, allowedOrigin := range c.AllowedOrigins {
+			fmt.Println(allowedOrigin)
+		}
 		// Check if origin is allowed
 		if c.isOriginAllowed(origin) {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
